98bf36a0db2b3ec3c122a5e6726b8605d70938a5
Wajih Katrou <wajih.katrou@ensi-uma.tn>
2019-10-10 17:07:03 +0200

    Adding the recipe list feature

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  	selector: 'app-recipe-list',
+  	templateUrl: './recipe-list.component.html',
+  	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+	recipes: Recipe[] = [
+		new Recipe("firstRecipe", "This is the first recipe", "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg")
+	];
 
-  ngOnInit() {
-  }
+  	constructor() { }
+
+  	ngOnInit() {
+  	}
 
 }
