508f8d9141dc9f52230aa587ead4fad1d0ee0dbc
Wajih Katrou <wajih.katrou@ensi-uma.tn>
2019-10-10 03:01:30 +0200

    Adding the RecipeListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component'; 
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule
