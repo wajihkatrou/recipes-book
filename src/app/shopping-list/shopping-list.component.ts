eafdc5de3e8f32d430be9aca700a109c7e539343
Wajih Katrou <wajih.katrou@ensi-uma.tn>
2019-10-10 18:05:24 +0200

    Adding the shopping list section

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+	selector: 'app-shopping-list',
+  	templateUrl: './shopping-list.component.html',
+  	styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
 
-  constructor() { }
+	ingredients : Ingredient[] = [
+		new Ingredient('Apples', 5),
+		new Ingredient('Bananas', 7),
+	];
+  	constructor() { }
 
-  ngOnInit() {
+  	ngOnInit() {
   }
 
 }
