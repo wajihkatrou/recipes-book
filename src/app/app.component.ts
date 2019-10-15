175e05b8d98fc209cda0fa177475178281cf2fd0
Wajih Katrou <wajih.katrou@ensi-uma.tn>
2019-10-15 15:39:32 +0200

    Adding navigation to header using ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  	selector: 'app-root',
+  	templateUrl: './app.component.html',
+  	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+	loadedPage = 'recipe';
+
+	onNavigation(page: string){
+		this.loadedPage = page;
+	}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Output } from '@angular/core'
 
 @Component({
 	selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core'
 })
 
 export class HeaderComponent{
-	
+	@Output() selectedPage = new EventEmitter<string>();
+
+	onSelect(page: string){
+		this.selectedPage.emit(page);
+	}
 }
